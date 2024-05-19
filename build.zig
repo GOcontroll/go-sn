6254ae2c454a149bce329935f0cfcf5689bea8ba
SpieringsAE <maud_spierings@hotmail.com>
2024-05-19 09:17:51 +0200

    updated build.zig to zig 0.12.0

--- build.zig
+++ build.zig
@@ -1,20 +1,22 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .aarch64,
+        .os_tag = .linux,
+        .abi = .gnu,
+        .glibc_version = std.SemanticVersion{ .major = 2, .minor = 31, .patch = 0 },
+    });
     const exe = b.addExecutable(.{
         .name = "go-sn",
-        .root_source_file = .{ .path = "go-sn.c" },
-        .target = std.zig.CrossTarget{
-            .os_tag = .linux,
-            .cpu_arch = .aarch64,
-            .abi = .gnu,
-            .glibc_version = std.SemanticVersion{ .major = 2, .minor = 31, .patch = 0 },
-        },
+        //.root_source_file = .{ .path = "go-sn.c" },
+        .target = target,
         .version = .{ .major = 1, .minor = 0, .patch = 0 },
         .optimize = std.builtin.OptimizeMode.ReleaseSmall,
+        .link_libc = true,
+        .strip = true,
     });
-
-    exe.linkLibC();
+    exe.addCSourceFile(std.Build.Module.CSourceFile{ .file = .{ .path = "go-sn.c" } });
 
     b.installArtifact(exe);
 }
