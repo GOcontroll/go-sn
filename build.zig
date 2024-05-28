dfd51ae4b383db73a7b8bfd6a023a778cd151fba
SpieringsAE <maud_spierings@hotmail.com>
2024-05-28 08:36:02 +0200

    small improvements to the build file

--- build.zig
+++ build.zig
@@ -5,18 +5,17 @@ pub fn build(b: *std.Build) void {
         .cpu_arch = .aarch64,
         .os_tag = .linux,
         .abi = .gnu,
-        .glibc_version = std.SemanticVersion{ .major = 2, .minor = 31, .patch = 0 },
+        .glibc_version = .{ .major = 2, .minor = 31, .patch = 0 },
     });
     const exe = b.addExecutable(.{
         .name = "go-sn",
-        //.root_source_file = .{ .path = "go-sn.c" },
         .target = target,
         .version = .{ .major = 1, .minor = 0, .patch = 0 },
-        .optimize = std.builtin.OptimizeMode.ReleaseSmall,
+        .optimize = .ReleaseSmall,
         .link_libc = true,
         .strip = true,
     });
-    exe.addCSourceFile(std.Build.Module.CSourceFile{ .file = .{ .path = "go-sn.c" } });
+    exe.addCSourceFile(.{ .file = .{ .path = "go-sn.c" } });
 
     b.installArtifact(exe);
 }
