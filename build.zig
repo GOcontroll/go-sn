f07811b3c4ada28fa8a7268ca432379030da7a29
SpieringsAE <maud_spierings@hotmail.com>
2024-08-12 17:57:21 +0200

    now builds the C version with zig 0.10.1

--- build.zig
+++ build.zig
@@ -7,46 +7,42 @@ const BuildType = enum {
 
 const version = "1.0.2";
 
-pub fn build(b: *std.Build) !void {
+pub fn build(b: *std.build.Builder) !void {
     const options = b.addOptions();
 
-    const build_type = b.option(BuildType, "build_type", "Whether to build the C or Zig version, default is Zig") orelse .zig;
-
-    const target = b.resolveTargetQuery(.{
-        .cpu_arch = .aarch64,
-        .os_tag = .linux,
-        .abi = .gnu,
-        .glibc_version = .{ .major = 2, .minor = 31, .patch = 0 },
+    const build_type = b.option(BuildType, "build_type", "Whether to build the C or Zig version, default is C") orelse .c;
+    const target = b.standardTargetOptions(.{
+        .default_target = .{
+            .abi = .gnu,
+            .os_tag = .linux,
+            .glibc_version = .{ .major = 2, .minor = 31, .patch = 0 },
+            .cpu_arch = .aarch64,
+        },
     });
-
-    const semver = try std.SemanticVersion.parse(version);
+    const semver = try std.builtin.Version.parse(version);
+    //const semver = try std.SemanticVersion.parse(version);
 
     switch (build_type) {
         .zig => { //build the zig version
-            const exe = b.addExecutable(.{
-                .name = "go-sn",
-                .root_source_file = .{ .cwd_relative = "go-sn.zig" }, //the zig version of this program is a bit more than twice the size 6kb vs 14 kb
-                .target = target,
-                .version = semver,
-                .optimize = .ReleaseSmall,
-                .link_libc = true,
-                .strip = true,
-            });
-            options.addOption([:0]const u8, "version", version); //add executable version to the "config" module
-            exe.root_module.addOptions("config", options);
+            var exe = b.addExecutable("go-sn", "go-sn.zig");
+
+            exe.target = target;
+            exe.linkLibC();
+            exe.build_mode = .ReleaseSmall;
+            exe.strip = true;
+            exe.version = semver;
+            options.addOption([]const u8, "version", version);
+            exe.addOptions("config", options);
             b.installArtifact(exe);
         },
         .c => { //build the c version
-            const exe = b.addExecutable(.{
-                .name = "go-sn",
-                .target = target,
-                .version = semver,
-                .optimize = .ReleaseSmall,
-                .link_libc = true,
-                .strip = true,
-            });
-            exe.root_module.addCMacro("VERSION", version); //add executable version as a #define
-            exe.addCSourceFile(.{ .file = .{ .cwd_relative = "go-sn.c" } });
+            var exe = b.addExecutable("go-sn", "go-sn.c");
+            exe.target = target;
+            exe.linkLibC();
+            exe.build_mode = .ReleaseSmall;
+            exe.strip = true;
+            exe.version = semver;
+            exe.defineCMacro("VERSION", version);
             b.installArtifact(exe);
         },
     }
